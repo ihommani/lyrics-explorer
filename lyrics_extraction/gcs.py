5dca69441c226ef12811a79498adb481e4966244
Ismael Hommani <>
2022-01-06 18:31:52 +0100

    POC getting lyrics from a file in GCS

---
+++ lyrics_extraction/gcs.py
@@ -0,0 +1,33 @@
+import re
+
+# Imports the Google Cloud client library
+from google.cloud import storage
+
+from bs4 import BeautifulSoup
+from bs4.element import SoupStrainer
+
+# Instantiates a client
+storage_client = storage.Client()
+
+# The name for the new bucket
+bucket_name = "test-ihommani"
+bucket = storage_client.get_bucket(bucket_name)
+
+blob = bucket.blob("lyrics.html")
+
+
+with open("/tmp/lyrics.html", 'wb') as file_obj:
+    blob.download_to_file(file_obj)
+
+with open("/tmp/lyrics.html", 'r') as file_obj:
+    soup = BeautifulSoup(file_obj, 'html.parser')
+
+def is_lyrics_container(tag):
+    return  tag.name == 'div' and tag.get('data-lyrics-container') == "true"
+
+lyrics_cleaning_expression = '\\[.*?\\]|\\n'
+
+lyrics_elements =  [re.sub(lyrics_cleaning_expression, '', i.get_text(' ').strip()) for i in soup.find_all(is_lyrics_container)]
+
+print(lyrics_elements)
+
