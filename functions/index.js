2ef785e673cd77c1e74335d36cd9a29bd64fbbd5
Ismael Hommani <>
2022-01-08 18:21:22 +0100

    Complete message payload
    Add more info to the song title.
    Use of Generator to merge data from different endpoints

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const SpotifyWebApi = require('spotify-web-api-node');
 const {PubSub} = require('@google-cloud/pubsub');
 const axios = require('axios');
+const jq = require('node-jq')
 
 const pubSubClient = new PubSub();
 const topic = pubSubClient.topic('opinion')
@@ -38,10 +39,12 @@ spotifyApi.clientCredentialsGrant().then(
 
 // main
 exports.getSongsMetadata = async (req, res) => {
-  const artistOfInterest = req.query.artist || 'Diams'
-  let songs = await sendMessages(artistOfInterest)
-  await Promise.all(songs.pubsubPromises)
-  res.send(songs.songs);
+  const artistOfInterest = req.query.artist || 'Diams' // TODO: reject with error on missing artist
+  
+  for await (let payloads of generatePayloads(artistOfInterest)) {
+        Promise.all(payloads.map(payload => topic.publishMessage({json: payload})))
+    }
+  res.send('hello')
 };
 
 exports.getLyricsHTML = async (req, res) => {
@@ -49,47 +52,57 @@ exports.getLyricsHTML = async (req, res) => {
   let {data} = await geniusApi.get('/search', {params: {q : 'Kendrick'}})
   if (data.response.hits.length === 0) return null;
   let htmlUrl = data.response.hits[0].result.url
+  // TODO: call the content and push it to GCS
   res.send(`<a href=${htmlUrl}>htmlUrl</a>`)
 }
 
-async function sendMessages(artistOfInterest) {
+async function* generatePayloads(artistOfInterest) {
+  const trackNameFilter = '.[] |= {"name": .name}'
+  let pubSubMessagePayload
 
-  let songs = await getTracksFromArtist(artistOfInterest)
-  .then(resp => resp
-    .map(e => e.body)
-    .reduce((acc, x) => acc.concat(x.items), []) // Should work with a flatMap starting node11
-    .map(e => e.name))
-  .catch(err => console.log(err))
+  for await (let albumContainer of generateAlbumContainers(artistOfInterest)){
+    let albumSongs = await albumContainer.tracks
+    let tracks = await jq.run(trackNameFilter, albumSongs.body.items, { input: 'json', output: 'json' })
 
-  let [genre, artist, date, album, song] =  ['rap', 'diams', '2000-11-06', 'Melanie', 'la boulette']
-
-  var promises = songs
-                 .map((track) => ({'song': track, 'artist': artist, 'genre': genre, 'date': date, 'album': album, 'song': song}))
-                 .map(jsonObject => topic.publishMessage({json: jsonObject}))
-
-  return {'songs': songs, 'pubsubPromises': promises }
+    pubSubMessagePayload = tracks.map((track) => ({ 'artist': albumContainer.artist.name, 'genre': albumContainer.artist.genres,
+                                                       'release_date': albumContainer.album.release_date, 'album_name': albumContainer.album.name,
+                                                       'track': track.name }))
+    yield pubSubMessagePayload
+  }
 }
 
-async function getTracksFromArtist(artistName) {
-  let artistId = await getArtistId(artistName)
-  let albumIds = await getAlbumIds(artistId)
-  let trackPromises = albumIds.map(id => getTracksFromAlbum(id))
-  return Promise.all(trackPromises)
+async function* generateAlbumContainers(artistName) {
+  let artist = await getArtistInformation(artistName)
+  let albums = await getAlbumInformations(artist.id)
+
+  for(album of albums) {
+    yield ({'artist': artist, 'album': album, 'tracks': getTracksFromAlbum(album.id) })
+  }
 }
 
-function getArtistId(artistName) {
-  return spotifyApi.searchArtists(artistName)
-    .then(resp => resp.body.artists.items[0].id)
+async function getArtistInformation(artistName) {
+  //https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artist
+  const artistMetaFilter = '. | {name: .name, id:.id, genres: .genres}'
+
+  let artist = await spotifyApi.searchArtists(artistName).then(resp => resp.body.artists.items[0])
+  return jq.run(artistMetaFilter, artist, { input: 'json', output: 'json' })
+}
+
+async function getAlbumInformations(artistId) {
+  // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-album
+  // oddly enough we cannot get tracks within the reponse body as explicited in the documentation model
+  const albumMetaFilter = '.[] |= {id: .id, release_date:.release_date, name:.name}'
+
+  let albums = await spotifyApi.getArtistAlbums(artistId) // warning point for throtling
+                              .then(data => data.body)
+                              .then(body => body.items)
+                              .catch(err => console.log(err))
+
+  return jq.run(albumMetaFilter, albums, { input: 'json', output: 'json' })
 }
 
 function getTracksFromAlbum(albumId) {
+  // https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-albums-tracks
   return spotifyApi.getAlbumTracks(albumId)
-}
-
-function getAlbumIds(artistId) {
-  return spotifyApi.getArtistAlbums(artistId, { limit: 2, offset: 0 }) // warning point for throtling
-    .then(data => data.body)
-    .then(body => body.items)
-    .then(items => items.map(element => element.id))
-    .catch(err => console.log(err))
+                  .catch(err => console.log(err))
 }
